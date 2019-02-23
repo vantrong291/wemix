4944aa2e935e46d1ef990a3bccc921102bc5a3e1
vantrong291 <vantrong291@gmail.com>
2019-02-23 14:08:41 +0700

    update FCM

--- App.js
+++ App.js
@@ -19,17 +19,116 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  async componentDidMount() {
+    this.checkPermission();
+    this.createNotificationListeners();
+  }
+
+  //1
+  async checkPermission() {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      this.getToken();
+    } else {
+      this.requestPermission();
+    }
+  }
+
+  //3
+  async getToken() {
+    let fcmToken = await AsyncStorage.getItem('fcmToken', value);
+    if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+        // user has a device token
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+    }
+  }
+
+  //2
+  async requestPermission() {
+    try {
+      await firebase.messaging().requestPermission();
+      // User has authorised
+      this.getToken();
+    } catch (error) {
+      // User has rejected permissions
+      console.log('permission rejected');
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        {/*<Text style={styles.welcome}>Welcome to React Native!</Text>*/}
-        {/*<Text style={styles.instructions}>To get started, edit App.js</Text>*/}
-        {/*<Text style={styles.instructions}>{instructions}</Text>*/}
-        <Login/>
-      </View>
-
+        <View style={{flex: 1}}>
+          <Text>Welcome to React Native!</Text>
+        </View>
     );
   }
+  ////////////////////// Add these methods //////////////////////
+
+  //Remove listeners allocated in createNotificationListeners()
+  componentWillUnmount() {
+    this.notificationListener();
+    this.notificationOpenedListener();
+  }
+
+  async createNotificationListeners() {
+    /*
+    * Triggered when a particular notification has been received in foreground
+    * */
+    this.notificationListener = firebase.notifications().onNotification((notification) => {
+      const { title, body } = notification;
+      this.showAlert(title, body);
+    });
+
+    /*
+    * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+    * */
+    this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+      const { title, body } = notificationOpen.notification;
+      this.showAlert(title, body);
+    });
+
+    /*
+    * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+    * */
+    const notificationOpen = await firebase.notifications().getInitialNotification();
+    if (notificationOpen) {
+      const { title, body } = notificationOpen.notification;
+      this.showAlert(title, body);
+    }
+    /*
+    * Triggered for data only payload in foreground
+    * */
+    this.messageListener = firebase.messaging().onMessage((message) => {
+      //process data message
+      console.log(JSON.stringify(message));
+    });
+  }
+
+  showAlert(title, body) {
+    Alert.alert(
+        title, body,
+        [
+          { text: 'OK', onPress: () => console.log('OK Pressed') },
+        ],
+        { cancelable: false },
+    );
+  }
+
+
+  // render() {
+  //   return (
+  //     <View style={styles.container}>
+  //       {/*<Text style={styles.welcome}>Welcome to React Native!</Text>*/}
+  //       {/*<Text style={styles.instructions}>To get started, edit App.js</Text>*/}
+  //       {/*<Text style={styles.instructions}>{instructions}</Text>*/}
+  //       <Login/>
+  //     </View>
+  //
+  //   );
+  // }
 }
 
 const styles = StyleSheet.create({
--- android/app/src/main/java/com/nanaapp/MainApplication.java
+++ android/app/src/main/java/com/nanaapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.nanaapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -10,6 +11,10 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
+
+import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -22,7 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RNFirebasePackage()
       );
     }
 
@@ -41,5 +47,14 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
+  }
+
+  protected List<ReactPackage> getPackages() {
+    return Arrays.<ReactPackage>asList(
+   new MainReactPackage(),
+   new RNFirebasePackage(),
+   new RNFirebaseMessagingPackage(),
+   new RNFirebaseNotificationsPackage()
+   );
   }
 }
