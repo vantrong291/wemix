30b96441fb88bed93135b620e18fb99ee9961482
vantrong291 <vantrong291@gmail.com>
2019-04-08 19:43:17 +0700

    edit service theme

--- service/routes/index.js
+++ service/routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dashboard', { title: 'Express' });
+  res.render('dashboard', { title: 'Express', dashboardClass: "active", iconClass: "", usersClass: "", userClass: "", notificationsClass: "" });
+});
+
+router.get('/icons', function(req, res, next) {
+  res.render('icons', { title: 'Express', iconClass: "active", dashboardClass: "", usersClass: "", userClass: "", notificationsClass: ""  });
+});
+
+router.get('/users', function(req, res, next) {
+  res.render('tables', { title: 'Express', usersClass: "active", dashboardClass: "", iconClass: "", userClass: "", notificationsClass: ""  });
+});
+
+router.get('/user', function(req, res, next) {
+  res.render('user', { title: 'Express', userClass: "active", dashboardClass: "", iconClass: "", usersClass: "", notificationsClass: ""   });
+});
+
+router.get('/notifications', function(req, res, next) {
+  res.render('notifications', { title: 'Express', notificationsClass: "active", dashboardClass: "", iconClass: "", usersClass: "", userClass: ""});
 });
 
 module.exports = router;
