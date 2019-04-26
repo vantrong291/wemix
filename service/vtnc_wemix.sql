72d7b03b97a59a5d41f6d3abdde984dc3433f392
vantrong291 <vantrong291@gmail.com>
2019-04-26 17:50:36 +0700

    update service db

--- service/vtnc_wemix.sql
+++ service/vtnc_wemix.sql
@@ -15,7 +15,6 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
 DROP DATABASE IF EXISTS vtnc_wemix;
 CREATE DATABASE vtnc_wemix CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE vtnc_wemix;
@@ -40,7 +39,7 @@ CREATE TABLE `SequelizeMeta` (
 
 LOCK TABLES `SequelizeMeta` WRITE;
 /*!40000 ALTER TABLE `SequelizeMeta` DISABLE KEYS */;
-INSERT INTO `SequelizeMeta` VALUES ('20190409032641-groups.js');
+INSERT INTO `SequelizeMeta` VALUES ('20190409032641-groups.js'),('20190426102410-users.js');
 /*!40000 ALTER TABLE `SequelizeMeta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -58,7 +57,7 @@ CREATE TABLE `favourite_tracks` (
   PRIMARY KEY (`id`),
   KEY `belong_to_user` (`belong_to_user`),
   CONSTRAINT `favourite_tracks_ibfk_1` FOREIGN KEY (`belong_to_user`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,6 +66,7 @@ CREATE TABLE `favourite_tracks` (
 
 LOCK TABLES `favourite_tracks` WRITE;
 /*!40000 ALTER TABLE `favourite_tracks` DISABLE KEYS */;
+INSERT INTO `favourite_tracks` VALUES (4,'12343',7),(5,'12343',7),(6,'12343',7),(7,'12343',7),(8,'12343',7),(9,'12343',7),(10,'12343',7),(11,'12343',7),(12,'12343',7);
 /*!40000 ALTER TABLE `favourite_tracks` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -112,7 +112,7 @@ CREATE TABLE `playlists` (
   UNIQUE KEY `name` (`name`),
   KEY `belong_to_user` (`belong_to_user`),
   CONSTRAINT `playlists_ibfk_1` FOREIGN KEY (`belong_to_user`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -121,6 +121,7 @@ CREATE TABLE `playlists` (
 
 LOCK TABLES `playlists` WRITE;
 /*!40000 ALTER TABLE `playlists` DISABLE KEYS */;
+INSERT INTO `playlists` VALUES (3,'My playlist','12341234123412341234',7),(4,'My playlist 01','1234',7);
 /*!40000 ALTER TABLE `playlists` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -135,11 +136,14 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `token` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `group` int(11) DEFAULT NULL,
+  `displayName` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `avatarPath` varchar(300) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `token` (`token`),
   KEY `group` (`group`),
   CONSTRAINT `users_ibfk_1` FOREIGN KEY (`group`) REFERENCES `groups` (`id`) ON DELETE SET NULL ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -148,7 +152,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (4,'1234s',2),(6,'12341',2),(7,'12343',2);
+INSERT INTO `users` VALUES (4,'1234s',2,NULL,NULL,NULL),(6,'12341',2,NULL,NULL,NULL),(7,'12343',2,NULL,NULL,NULL),(8,'nes2vkWBzoUPRmu2FCSjBJy49Q33',2,NULL,NULL,NULL),(9,'hqM0xTy8txRSY5DQgJC6gaOJv8D2',2,'Trọng Văn','https://lh6.googleusercontent.com/-oC2DKgp660w/AAAAAAAAAAI/AAAAAAAAADE/UqacFe--tTM/photo.jpg','vantrong291@gmail.com');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -161,4 +165,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-04-09 14:39:53
+-- Dump completed on 2019-04-26 17:50:02
